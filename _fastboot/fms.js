20831928055f4a74751c6a69433c21354ec60f47
Grifree <1149290648@qq.com>
2016-11-08 18:21:23 +0800

    待测试ie

---
+++ _fastboot/fms.js
@@ -0,0 +1,27 @@
+var fms = require('fms')
+
+fms.run({
+    port: 38770,
+    static: './output',
+    view: {
+        server: 'http://127.0.0.1:38771',
+    },
+    ajax: {
+        res: {
+            ok: {
+                status: '',
+                code: 0
+            },
+            err: false
+        }
+    }
+})
+
+fms.ajax({
+	type:'post',
+	url:'/',
+	res:{
+		ok:'ok',
+		err:'err'
+	}
+})
